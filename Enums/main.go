7578fbc7fd0aa3f6640fff7f7b120b9d5413292c
Adifarooque <Adilfarq11@gmail.com>
2023-09-18 20:13:31 +0530

    Initial Commit

--- Enums/main.go
+++ Enums/main.go
@@ -1,9 +1,24 @@
 package main
 
+import "fmt"
+
 type WeaponType int
 
+func getDamage(weapontype string) int {
+	switch weapontype {
+	case "Axe":
+		return 100
+	case "Woodenstick":
+		return 10
+	case "Knife":
+		return 50
+	case "Rolling pin":
+		return 70
+	default:
+		panic("Weapon Does not exist..")
+	}
+}
 
-
-func main(){
-	
+func main() {
+	fmt.Println("Damage Of Weapon ", getDamage("Knife"))
 }
