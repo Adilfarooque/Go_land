1ab79400734f61008cbe6e4155018204e32c3288
Adifarooque <Adilfarq11@gmail.com>
2023-09-18 12:07:58 +0530

    Intial Commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+type WeaponTypes int
+
+func getDamage(weapontype string)int{
+	switch weapontype{
+	case "Axe":
+		return 100
+	case "Sword":
+		return 90
+	case "WoodenStick":
+		return 10
+	case "Knife":
+		return 40
+	default:
+		panic("Weapon Does Not Exist..")
+	}
+}
+
+func main(){
+	fmt.Println("Damage Of The Weapon:",getDamage("Knife"))
+}
