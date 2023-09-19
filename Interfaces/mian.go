90089e862356cfc96625569c70d6bbdd80cde006
Adifarooque <Adilfarq11@gmail.com>
2023-09-19 11:08:23 +0530

    Added CR7 struct & reciver func

--- Interfaces/mian.go
+++ Interfaces/mian.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+type CR7 struct {
+	stamina int
+	power   int
+	sui     int
+}
+
+func (c CR7) KickBall()int {
+	return c.stamina + c.power * c.sui
+}
+
+
 func main() {
 
 }
