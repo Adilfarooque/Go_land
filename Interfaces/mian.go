4357379971ab7968cc8870bef2aafe885c53c994
Adifarooque <Adilfarq11@gmail.com>
2023-09-19 11:16:03 +0530

    Created a receiver for CR7

--- Interfaces/mian.go
+++ Interfaces/mian.go
@@ -12,15 +12,6 @@ func (c CR7) KickBall()int {
 	return c.stamina + c.power * c.sui
 }
 
-type Messi struct{
-	stamina int
-	power int
-	curve int
-}
-
-func (m Messi)KickBall()int{
-	return m.stamina + m.power * m.curve
-}
 
 func main() {
 
