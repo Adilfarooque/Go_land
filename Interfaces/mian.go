3995f4bef7c637c98b830d28d18b6489ceda7c76
Adifarooque <Adilfarq11@gmail.com>
2023-09-19 14:28:26 +0530

    Usage of interfaces

--- Interfaces/mian.go
+++ Interfaces/mian.go
@@ -1,36 +1,65 @@
 package main
 
-import "fmt"
-
-type Finishers interface{
+import (
+	"fmt"
+	"math/rand"
+)
+type Player interface{
 	KickBall()
 }
 
+type FootballPlayer struct {
+	stamina int
+	power   int
+}
 type CR7 struct {
 	stamina int
 	power   int
 	sui     int
 }
 
-func (c CR7) KickBall()int {
-	return c.stamina + c.power * c.sui
+func (c CR7) KickBall() {
+	shot := c.stamina + c.power*c.sui
+	fmt.Println("Cr7 fentasitc acrobatic finish...", shot)
 }
 
-type Messi struct{
-	stamina int 
-	power int
-	curve int
+type Messi struct {
+	stamina int
+	power   int
+	curve   int
 }
 
-func (m Messi)KickBall()int{
-	return m.stamina + m.power * m.curve
+func (m Messi) KickBall() {
+	shot := m.stamina + m.power*m.curve
+	fmt.Println("Messi Curling to the Corner.....have a fabiolous freekick.", shot)
 }
 
-func Shot(e Finishers){
-	e.KickBall()
+
+func (f FootballPlayer) KickBall() {
+	shot := f.stamina + f.power
+	fmt.Println("I'm kicking the ball", shot)
 }
 
 
 func main() {
-
+	team := make([]Player, 11)
+	for i := 0; i < len(team); i++ {
+		team[i] = FootballPlayer{
+			stamina: rand.Intn(10),
+			power:   rand.Intn(10),
+		}
+	}
+	team[len(team)-2]=Messi{
+		stamina: 8,
+		power: 9,
+		curve: 10,
+	}
+	team[len(team)-1]=CR7{
+		stamina: 10,
+		power : 10,
+		sui :10,
+	}
+	for i := 0; i < len(team); i++ {
+		team[i].KickBall()
+	}
 }
---
+++ Interfaces/tempCodeRunnerFile.go
@@ -0,0 +1,5 @@
+type CR7 struct {
+// 	stamina int
+// 	power   int
+// 	sui     int
+// }
