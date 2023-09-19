aad8ac286e5acb86464c77e0eb75494a6c403f47
Adifarooque <Adilfarq11@gmail.com>
2023-09-19 11:13:02 +0530

    Added Leo Messi as reciver

--- Interfaces/mian.go
+++ Interfaces/mian.go
@@ -12,6 +12,15 @@ func (c CR7) KickBall()int {
 	return c.stamina + c.power * c.sui
 }
 
+type Messi struct{
+	stamina int
+	power int
+	curve int
+}
+
+func (m Messi)KickBall()int{
+	return m.stamina + m.power * m.curve
+}
 
 func main() {
 
